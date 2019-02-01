438e694916e72d288c339de6a5d2373a0640049e
omgbtc <omgbtc>
2019-02-01 12:12:04 -0500

    added removing volumes

--- reset-all.sh
+++ reset-all.sh
@@ -9,4 +9,5 @@ docker-compose -f logspout.yml down
 docker-compose -f manager.yml down
 docker-compose -f update-manager.yml down
 docker-compose -f welcome.yml down
+docker volume rm applications_accounts applications_bitcoind-data applications_channel-data applications_lnd-data applications_logs applications_settings
 popd
